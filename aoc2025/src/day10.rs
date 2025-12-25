19e0ec773a4fba4b57ff485d681a891168a95436
Dennis Ruppel <crap-dennis@gmx.net>
2025-12-25 04:51:35 +0100

    speedup and cleanup day 10

--- aoc2025/src/day10.rs
+++ aoc2025/src/day10.rs
@@ -1,22 +1,57 @@
-use std::{collections::HashMap, num::ParseIntError};
+use std::{collections::HashMap, num::ParseIntError, rc::Rc};
 
 use bitvec::prelude as bv;
+use rayon::prelude::*;
 use thiserror::Error;
 
-const MAX_LAMPS: usize = 64;
-type BitMask = bv::BitArr!(for MAX_LAMPS);
+const MAX_BITS: usize = 64;
+type BitArr = bv::BitArr!(for MAX_BITS);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone)]
+struct BitMask {
+    inner: BitArr,
+    used: u8,
+}
+
+impl BitMask {
+    fn new(used: usize) -> Option<Self> {
+        (used <= MAX_BITS).then_some(Self {
+            inner: bv::bitarr![0; MAX_BITS],
+            used: used as u8,
+        })
+    }
+}
+
+impl std::ops::Deref for BitMask {
+    type Target = bv::BitSlice;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner[..self.used as usize]
+    }
+}
+
+impl std::ops::DerefMut for BitMask {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner[..self.used as usize]
+    }
+}
+
+impl std::ops::BitXorAssign for BitMask {
+    fn bitxor_assign(&mut self, rhs: Self) {
+        self.inner ^= rhs.inner;
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Diagram {
     target: BitMask,
-    lamps: usize,
 }
 
 #[derive(Debug, Error)]
 pub enum DiagramParseError {
     #[error("expect format '[(.|#)+]', got '{0}'")]
     WrongFormat(String),
-    #[error("can only handle {MAX_LAMPS} lights, but found {0}")]
+    #[error("can only handle {MAX_BITS} lights, but found {0}")]
     TooManyLamps(usize),
 }
 
@@ -33,11 +68,10 @@ impl Diagram {
         }
 
         let lamps = input.len();
-        if lamps > MAX_LAMPS {
+        let Some(mut target) = BitMask::new(lamps) else {
             return Err(DiagramParseError::TooManyLamps(lamps));
-        }
+        };
 
-        let mut target = bv::bitarr![0; MAX_LAMPS];
         for (i, c) in input.chars().enumerate() {
             match c {
                 '.' => (),
@@ -46,18 +80,18 @@ impl Diagram {
             }
         }
 
-        Ok(Self { target, lamps })
+        Ok(Self { target })
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Button {
     flips: BitMask,
 }
 
 #[derive(Debug, Error)]
 pub enum ButtonParseError {
-    #[error("can only handle {MAX_LAMPS} lights, but provided {0}")]
+    #[error("can only handle {MAX_BITS} lights, but provided {0}")]
     TooManyLamps(usize),
     #[error("expect format '(<number>(,<number>)*)', got '{0}'")]
     WrongFormat(String),
@@ -70,10 +104,10 @@ pub enum ButtonParseError {
 }
 
 impl Button {
-    fn parse(input: &str, lights: usize) -> Result<Self, ButtonParseError> {
-        if lights > MAX_LAMPS {
-            return Err(ButtonParseError::TooManyLamps(lights));
-        }
+    fn parse(input: &str, lamps: usize) -> Result<Self, ButtonParseError> {
+        let Some(mut flips) = BitMask::new(lamps) else {
+            return Err(ButtonParseError::TooManyLamps(lamps));
+        };
 
         if input.len() < 3 {
             return Err(ButtonParseError::WrongFormat(input.to_string()));
@@ -85,27 +119,26 @@ impl Button {
             _ => return Err(ButtonParseError::WrongFormat(input.to_string())),
         }
 
-        let flips = input
+        let flip_numbers = input
             .split(',')
             .map(|v| {
                 v.parse()
                     .map_err(|e| ButtonParseError::NotANumber(v.to_string(), e))
-                    .and_then(|v| match v < lights {
+                    .and_then(|v| match v < lamps {
                         true => Ok(v),
-                        false => Err(ButtonParseError::NumberTooLarge(v, lights)),
+                        false => Err(ButtonParseError::NumberTooLarge(v, lamps)),
                     })
             })
             .collect::<Result<Vec<_>, _>>()?;
 
-        let mut button = bv::bitarr![0; MAX_LAMPS];
-        for flip in flips {
-            if button[flip] {
+        for flip in flip_numbers {
+            if flips[flip] {
                 return Err(ButtonParseError::FlippedTwice(flip));
             }
-            button.set(flip, true);
+            flips.set(flip, true);
         }
 
-        Ok(Self { flips: button })
+        Ok(Self { flips })
     }
 }
 
@@ -121,7 +154,7 @@ pub enum JoltageTargetParseError {
     WrongFormat(String),
     #[error("could not parse '{0}' as a number")]
     NotANumber(String, #[source] ParseIntError),
-    #[error("can only handle {MAX_LAMPS} lights, but found {0} joltages")]
+    #[error("can only handle {MAX_BITS} lights, but found {0} joltages")]
     TooManyLamps(usize),
 }
 
@@ -144,7 +177,7 @@ impl JoltageTarget {
                     .map_err(|e| JoltageTargetParseError::NotANumber(v.to_string(), e))
             })
             .collect::<Result<Vec<_>, _>>()?;
-        if joltages.len() > MAX_LAMPS {
+        if joltages.len() > MAX_BITS {
             return Err(JoltageTargetParseError::TooManyLamps(joltages.len()));
         }
 
@@ -152,17 +185,17 @@ impl JoltageTarget {
     }
 
     fn as_parity_diagram(&self) -> Diagram {
-        let lamps = self.0.0.len();
+        let n_lamps = self.0.0.len();
+        let mut target = BitMask::new(n_lamps).expect("lamp count valid by construction");
 
-        let mut target = bv::bitarr![0; MAX_LAMPS];
         for (i, take) in self.0.0.iter().map(|v| (v % 2) != 0).enumerate() {
             target.set(i, take);
         }
 
-        Diagram { target, lamps }
+        Diagram { target }
     }
 
-    fn reduced_with(&self, joltage: &Joltage) -> Option<JoltageTarget> {
+    fn reduce_by(&self, joltage: &Joltage) -> Option<JoltageTarget> {
         let mut reduced = self.clone();
         for (reduced_v, joltage_v) in reduced.0.0.iter_mut().zip(joltage.0.iter()) {
             *reduced_v = reduced_v.checked_sub(*joltage_v)?;
@@ -178,6 +211,10 @@ impl JoltageTarget {
             .map(|v| if v % 2 == 0 { Some(v / 2) } else { None })
             .collect::<Option<Vec<_>>>()?;
         Some(Self(Joltage(values)))
+    }
+
+    fn sum(&self) -> usize {
+        self.0.0.iter().sum()
     }
 }
 
@@ -212,11 +249,10 @@ impl Machine {
             .ok_or(MachineParseError::MissingPart)
             .and_then(|s| Ok(JoltageTarget::parse(s)?))?;
 
+        let n_lamps = diagram.target.len();
         let buttons = iter
             .enumerate()
-            .map(|(i, s)| {
-                Button::parse(s, diagram.lamps).map_err(|e| MachineParseError::Button(i, e))
-            })
+            .map(|(i, s)| Button::parse(s, n_lamps).map_err(|e| MachineParseError::Button(i, e)))
             .collect::<Result<Vec<_>, _>>()?;
         if buttons.is_empty() {
             return Err(MachineParseError::MissingPart);
@@ -232,22 +268,20 @@ impl Machine {
 
 #[derive(Debug, Clone)]
 struct BitPatternIterator {
-    lamps: usize,
     active: usize,
     next: Option<BitMask>,
 }
 
 impl BitPatternIterator {
     fn new(max: usize, active: usize) -> Self {
-        assert!(max < MAX_LAMPS);
         assert!(active <= max);
 
-        let mut pattern = bv::bitarr![0; MAX_LAMPS];
+        let mut pattern =
+            BitMask::new(max).expect("only allow {MAX_BITS} many bits, requested {max}");
         for i in 0..active {
             pattern.set(i, true);
         }
         Self {
-            lamps: max,
             active,
             next: Some(pattern),
         }
@@ -261,24 +295,21 @@ impl Iterator for BitPatternIterator {
         let res = self.next.take()?;
         let mut next = res;
 
-        let slice = &mut next[..self.lamps];
-        let Some(zero_pos) = slice.last_zero() else {
+        let Some(zero_pos) = next.last_zero() else {
             // special case: all bits are set, give a single element back
             return Some(res);
         };
-        let from_zero_slice = &slice[..zero_pos];
-        let Some(one_pos) = from_zero_slice.last_one() else {
+        let slice = &next[..zero_pos];
+        let Some(one_pos) = slice.last_one() else {
             // all bits right-aligned, iterator exhausted
             return Some(res);
         };
 
         // construct next pattern
-        slice.set(one_pos, false);
-        let replace_slice = &mut slice[one_pos + 1..];
-        let one_count = replace_slice.count_ones() + 1;
-        for i in 0..replace_slice.len() {
-            replace_slice.set(i, i < one_count);
-        }
+        next.set(one_pos, false);
+        let slice = &mut next[one_pos + 1..];
+        let one_count = slice.count_ones() + 1;
+        slice.fill_with(|i| i < one_count);
         assert!(next.count_ones() == self.active);
 
         self.next = Some(next);
@@ -287,111 +318,190 @@ impl Iterator for BitPatternIterator {
 }
 
 impl Machine {
-    fn make_button_mask_iterator(&self, diagram: Diagram) -> impl Iterator<Item = BitMask> {
-        (0..=self.buttons.len())
-            .flat_map(|presses| BitPatternIterator::new(self.buttons.len(), presses))
+    fn make_button_mask_iterator<'a>(
+        &'a self,
+        diagram: Diagram,
+    ) -> impl Iterator<Item = BitMask> + 'a {
+        let n_buttons = self.buttons.len();
+        (0..=n_buttons)
+            .flat_map(move |presses| BitPatternIterator::new(n_buttons, presses))
             .filter(move |button_mask| {
                 let acc = diagram.target;
-                let pattern = button_mask
-                    .iter()
-                    .enumerate()
-                    .fold(acc, |mut acc, (i, take)| {
-                        if *take {
-                            acc ^= self.buttons[i].flips;
-                        }
-                        acc
-                    });
+                let pattern =
+                    button_mask[..n_buttons]
+                        .iter()
+                        .enumerate()
+                        .fold(acc, |mut acc, (i, take)| {
+                            if *take {
+                                acc ^= self.buttons[i].flips;
+                            }
+                            acc
+                        });
                 pattern.count_ones() == 0
             })
     }
 
     fn presses_to_boot(&self) -> Option<usize> {
-        self.make_button_mask_iterator(self.diagram.clone())
+        self.make_button_mask_iterator(self.diagram)
             .next()
             .map(|mask| mask.count_ones())
     }
+}
 
-    fn joltage_from_button_mask(&self, mask: BitMask) -> Joltage {
-        let mut joltage = vec![0; self.diagram.lamps];
-        for (button, take) in mask.into_iter().take(self.buttons.len()).enumerate() {
-            if !take {
-                continue;
-            }
-            for (i, add) in self.buttons[button].flips.iter().enumerate() {
-                if *add {
-                    joltage[i] += 1;
+struct BifurcationState<'a> {
+    target: Rc<JoltageTarget>,
+    mask_iter: Box<dyn Iterator<Item = BitMask> + 'a>,
+    buttons_used: usize,
+    solution: Option<usize>,
+}
+
+enum BifurcationOutput<'a> {
+    Recurse {
+        next_target: Rc<JoltageTarget>,
+        state: BifurcationState<'a>,
+    },
+    Solved {
+        target: Rc<JoltageTarget>,
+        solution: Option<usize>,
+    },
+}
+
+enum BifurcationInput<'a> {
+    Start {
+        target: Rc<JoltageTarget>,
+    },
+    Continue {
+        state: BifurcationState<'a>,
+        solution: Option<usize>,
+    },
+}
+
+impl Machine {
+    fn bifurcation_recursion<'a>(&'a self, input: BifurcationInput<'a>) -> BifurcationOutput<'a> {
+        let state = match input {
+            BifurcationInput::Start { target } => {
+                if target.sum() == 0 {
+                    return BifurcationOutput::Solved {
+                        target,
+                        solution: Some(0),
+                    };
+                }
+
+                let diagram = target.as_parity_diagram();
+                let mask_iter = Box::new(self.make_button_mask_iterator(diagram));
+                BifurcationState {
+                    target,
+                    mask_iter,
+                    buttons_used: 0,
+                    solution: None,
                 }
             }
-        }
-        Joltage(joltage)
-    }
-
-    fn presses_to_configure_bifurcation(&self) -> Option<usize> {
-        use std::collections::hash_map::Entry;
-
-        struct StackState<T: Iterator<Item = BitMask>> {
-            target: JoltageTarget,
-            mask_iter: T,
-        }
-
-        let init = {
-            let diagram = self.joltage_target.as_parity_diagram();
-            let mask_iter = self.make_button_mask_iterator(diagram);
-            StackState {
-                target: self.joltage_target.clone(),
-                mask_iter,
+            BifurcationInput::Continue {
+                mut state,
+                solution,
+            } => {
+                if let Some(sol) = solution {
+                    let sol = state.buttons_used + 2 * sol;
+                    let sol = state.solution.map(|last| last.min(sol)).or(Some(sol));
+                    state.solution = sol;
+                }
+                state
             }
         };
-        let mut stack = Vec::from_iter([init]);
 
-        let mut cache: HashMap<JoltageTarget, Option<usize>> = HashMap::new();
+        let BifurcationState {
+            target,
+            mut mask_iter,
+            solution,
+            ..
+        } = state;
+
+        let next_target = mask_iter
+            .by_ref()
+            .flat_map(|mask| {
+                let joltage = self.joltage_from_button_mask(mask);
+                let target = target.reduce_by(&joltage)?;
+                let target = target.half_reqs().expect("made even above");
+                let buttons_used = mask.count_ones();
+                Some((target, buttons_used))
+            })
+            .next();
+
+        let Some((next_target, buttons_used)) = next_target else {
+            return BifurcationOutput::Solved { target, solution };
+        };
+        let next_target = Rc::new(next_target);
+
+        let state = BifurcationState {
+            target,
+            mask_iter,
+            buttons_used,
+            solution,
+        };
+        BifurcationOutput::Recurse { next_target, state }
+    }
+
+    fn joltage_from_button_mask(&self, mask: BitMask) -> Joltage {
+        let n_lamps = self.diagram.target.len();
+        let mut values = vec![0; n_lamps];
+        let indices = self
+            .buttons
+            .iter()
+            .zip(&*mask)
+            .filter_map(|(btn, take)| take.then_some(btn))
+            .flat_map(|btn| {
+                btn.flips
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, take)| take.then_some(i))
+            });
+        for i in indices {
+            values[i] += 1;
+        }
+        Joltage(values)
+    }
+
+    fn presses_to_configure(&self) -> Option<usize> {
+        use std::collections::hash_map::Entry;
+
+        let mut cache = HashMap::new();
+
+        let mut stack = Vec::new();
+        // init stack
         {
-            let zeros = vec![0; self.joltage_target.0.0.len()];
-            let stop_condition = JoltageTarget(Joltage(zeros));
-            cache.insert(stop_condition, Some(0));
+            let target = Rc::new(self.joltage_target.clone());
+            let input = BifurcationInput::Start { target };
+            let output = self.bifurcation_recursion(input);
+            stack.push(output);
         }
 
-        'stack: while let Some(mut state) = stack.pop() {
-            for mask in state.mask_iter.by_ref() {
-                let joltage = self.joltage_from_button_mask(mask);
-                let Some(target) = state.target.reduced_with(&joltage) else {
-                    continue;
-                };
-                let target = target.half_reqs().expect("made all even above");
-                if cache.contains_key(&target) {
-                    continue;
-                };
-
-                // solve sub problem first and then return to this problem
-                let diagram = target.as_parity_diagram();
-                let mask_iter = self.make_button_mask_iterator(diagram);
-                let new_state = StackState { target, mask_iter };
-                stack.push(state);
-                stack.push(new_state);
-                continue 'stack;
-            }
-
-            // all sub problem solutions exist in the cache
-            let diagram = state.target.as_parity_diagram();
-            let mask_iter = self.make_button_mask_iterator(diagram);
-            let solution = mask_iter
-                .into_iter()
-                .flat_map(|mask| {
-                    let joltage = self.joltage_from_button_mask(mask);
-                    let target = state.target.reduced_with(&joltage)?;
-                    let target = target.half_reqs().expect("made all even above");
-                    let solution = *cache.get(&target).expect("all solutions in cache");
-                    let solution = solution? * 2 + mask.count_ones();
-                    Some(solution)
-                })
-                .min();
-
-            match cache.entry(state.target.clone()) {
-                Entry::Occupied(entry) => assert_eq!(*entry.get(), solution),
-                Entry::Vacant(entry) => {
-                    entry.insert(solution);
+        while let Some(output) = stack.pop() {
+            match output {
+                BifurcationOutput::Solved { target, solution } => match cache.entry(target) {
+                    Entry::Occupied(entry) => assert_eq!(*entry.get(), solution),
+                    Entry::Vacant(entry) => {
+                        entry.insert(solution);
+                    }
+                },
+                BifurcationOutput::Recurse { next_target, state } => {
+                    match cache.get(&next_target) {
+                        Some(&solution) => {
+                            let input = BifurcationInput::Continue { state, solution };
+                            let output = self.bifurcation_recursion(input);
+                            stack.push(output);
+                        }
+                        None => {
+                            let input = BifurcationInput::Start {
+                                target: Rc::clone(&next_target),
+                            };
+                            let recursive_output = self.bifurcation_recursion(input);
+                            let last_output = BifurcationOutput::Recurse { next_target, state };
+                            stack.push(last_output);
+                            stack.push(recursive_output);
+                        }
+                    }
                 }
-            };
+            }
         }
 
         cache
@@ -441,14 +551,19 @@ impl Machines {
     }
 
     fn presses_to_configure_all(&self) -> Result<usize, ConfigureError> {
-        self.0.iter().enumerate().try_fold(0, |acc, (i, m)| {
-            println!("working on machine {i}");
-            let toggles = m
-                .presses_to_configure_bifurcation()
-                .ok_or(ConfigureError::UnsolvableMachine(i))?;
-            println!("machine {i} solved with {toggles}");
-            Ok(acc + toggles)
-        })
+        self.0
+            .par_iter()
+            .enumerate()
+            .try_fold(
+                || 0,
+                |acc, (i, m)| {
+                    let toggles = m
+                        .presses_to_configure()
+                        .ok_or(ConfigureError::UnsolvableMachine(i))?;
+                    Ok(acc + toggles)
+                },
+            )
+            .try_reduce(|| 0, |a, b| Ok(a + b))
     }
 }
 
